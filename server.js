2bc0e4995464bd42780bd2b4f2c584dcadbd97f4
Jack Long <putheara.long@gmail.com>
2020-06-10 18:15:05 -0400

    changed 'docs' to 'public'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("docs"));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://jack:jack123@ds031617.mlab.com:31617/heroku_qk7dqkng", {
   useNewUrlParser: true,

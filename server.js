11842b6b8ff4c3673f506674c4b6dbd13e981082
Jack Long <putheara.long@gmail.com>
2020-06-10 15:06:14 -0400

    added environment port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("docs"));
 
-mongoose.connect("mongodb://jack:jack123@ds031617.mlab.com:31617/heroku_qk7dqkng", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://jack:jack123@ds031617.mlab.com:31617/heroku_qk7dqkng", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
